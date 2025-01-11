997bcde97df1229a42d4668233447346b01dd8a3
GauravAnand437 <gauravanand437@gmail.com>
2025-01-12 05:11:12 +0530

    types configured

---
+++ src/components/templates/AnalysisWindow.tsx
@@ -0,0 +1,12 @@
+const AnalysisWindow = () => {
+  return (
+    <div className="flex flex-col h-fit self-center bg-[#000] rounded-lg p-4 pt-0 m-3 w-2/5 border border-[#222222]">
+      <div className="flex text-[#c9c9c9] py-4 font-medium text-lg">
+        Analysis Results
+      </div>
+      <div className="flex bg-[#ACD83A] w-full h-96 font-fustat rounded-lg"></div>
+    </div>
+  );
+};
+
+export default AnalysisWindow;
