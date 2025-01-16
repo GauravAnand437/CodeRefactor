2dd991b0327592c9d485045de0594b04a0e9c2a2
GauravAnand437 <gauravanand437@gmail.com>
2025-01-17 05:11:12 +0530

    types configured

---
+++ src/db/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI as string,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as mongoose.ConnectOptions
+    );
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
